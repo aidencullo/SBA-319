26e4ef1e5fce2da0350fa1aa1c8095913d8fd204
Aiden Cullo <aidencullo@gmail.com>
2024-09-13 08:35:24 -1000

    post and comment models

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ import express from 'express';
 import connectDB from './db.js';
 
 import User from './models/User.js';
+import Post from './models/Post.js';
+import Comment from './models/Comment.js';
 
 const app = express();
 
 app.get('/', async (req, res) => {
-  const result = await User.create({
+  const user = User.create({
     name: 'Test User',
   });
+  const post = Post.create({
+    name: 'Test Post',
+  });
+  const comment = Comment.create({
+    name: 'Test Comment',
+  });
+  const result = await Promise.all([user, post, comment]);
   res.send(result);
 });
 
