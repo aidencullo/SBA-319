b674f923c6e70739bb854bb4257e8a1c82c0b51a
Aiden Cullo <aidencullo@gmail.com>
2024-09-16 18:00:28 -0400

    add 404 handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import user from './routes/user.js';
 import post from './routes/post.js';
 import comment from './routes/comment.js';
 import errorHandler from './middleware/errorHandler.js';
+import error from './utils/error.js';
 
 const app = express();
 
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 app.use('/users', user);
 app.use('/posts', post);
 app.use('/comments', comment);
+
+app.use((req, res, next) => {
+  return next(error(404, 'Not Found'));
+});
 
 app.use(errorHandler);
 
