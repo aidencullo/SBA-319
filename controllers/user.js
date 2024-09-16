f857ba14066ba6db1f8be949b9dee9b94945cbdb
Aiden Cullo <aidencullo@gmail.com>
2024-09-16 18:57:22 -0400

    move route logic to controller

---
+++ controllers/user.js
@@ -0,0 +1,124 @@
+// controllers/userController.js
+import bcrypt from 'bcrypt';
+import User from '../models/User.js';
+import error from '../utils/error.js';
+
+export async function getUsers(req, res, next) {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    next(error(500, err.message));
+  }
+}
+
+export async function getUser(req, res, next) {
+  res.json(res.user);
+}
+
+export async function validateUser(req, res, next) {
+  if (req.params.id.length !== 24) {
+    return next(error(400, 'Invalid user ID'));
+  }
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      return next(error(404, 'User not found'));
+    }
+  } catch (err) {
+    return next(error(500, err.message));
+  }
+
+  res.user = user;
+  next();
+}
+
+
+export async function createUser(req, res, next) {
+  const { name, email, password, isAdmin } = req.body;
+
+  if (!password || password.length < 6) {
+    return next(error(400, 'Password must be at least 6 characters long'));
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword, isAdmin: isAdmin || false });
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(error(400, err.message));
+  }
+}
+
+export async function updateUser(req, res, next) {
+  const { name, email, password, isAdmin } = req.body;
+
+  if (name != null) res.user.name = name;
+  if (email != null) res.user.email = email;
+  if (password != null) res.user.password = await bcrypt.hash(password, 10);
+  if (isAdmin != null) res.user.isAdmin = isAdmin;
+
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    next(error(400, err.message));
+  }
+}
+
+export async function replaceUser(req, res, next) {
+  console.log('replaceUser');
+  const { name, email, password, isAdmin } = req.body;
+
+  res.user.name = name;
+  res.user.email = email;
+
+  if (password != null) {
+    res.user.password = await bcrypt.hash(password, 10);
+  }
+
+  res.user.isAdmin = isAdmin || res.user.isAdmin;
+
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    next(error(400, err.message));
+  }
+}
+
+export async function deleteUser(req, res, next) {
+  try {
+    await User.deleteOne({ _id: res.user.id });
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    next(error(500, err.message));
+  }
+}
+
+export async function deleteAllUsers(req, res, next) {
+  try {
+    await User.deleteMany();
+    res.json({ message: 'All users deleted' });
+  } catch (err) {
+    next(error(500, err.message));
+  }
+}
+
+export async function createInvalidUser(req, res, next) {
+  const invalidUser = new User({
+    name: '',
+    email: 'invalid-email',
+    password: 'short',
+    isAdmin: 'notabool'
+  });
+
+  try {
+    const result = await invalidUser.save();
+    res.status(201).json(result);
+  } catch (err) {
+    next(error(400, err.message));
+  }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,146 +1,26 @@
+// routes/userRoutes.js
 import express from 'express';
-import bcrypt from 'bcrypt';
-
-import User from '../models/User.js';
-import error from '../utils/error.js';
+import { 
+  getUsers, 
+  getUser, 
+  createUser, 
+  updateUser, 
+  replaceUser, 
+  deleteUser, 
+  deleteAllUsers, 
+  createInvalidUser,
+  validateUser,
+} from '../controllers/user.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    return next(error(500, err.message));
-  }
-});
-
-router.get('/:id', getUser, (req, res) => {
-  res.json(res.user);
-});
-
-router.post('/', async (req, res, next) => {
-  try {
-    const { name, email, password, isAdmin } = req.body;
-
-    if (!password || password.length < 6) {
-      return next(error(400, 'Password must be at least 6 characters long'));
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = new User({
-      name,
-      email,
-      password: hashedPassword,
-      isAdmin: isAdmin || false,
-    });
-
-    try {
-      const newUser = await user.save();
-      res.status(201).json(newUser);
-    } catch (err) {
-      return next(error(400, err.message));
-    }
-  } catch (err) {
-    return next(error(500, err.message));
-  }
-});
-
-router.patch('/:id', getUser, async (req, res, next) => {
-  const { name, email, password, isAdmin } = req.body;
-
-  if (name != null) {
-    res.user.name = name;
-  }
-  if (email != null) {
-    res.user.email = email;
-  }
-  if (password != null) {
-    res.user.password = await bcrypt.hash(password, 10);
-  }
-  if (isAdmin != null) {
-    res.user.isAdmin = isAdmin;
-  }
-
-  try {
-    const updatedUser = await res.user.save();
-    res.json(updatedUser);
-  } catch (err) {
-    return next(error(400, err.message));
-  }
-});
-
-router.put('/:id', getUser, async (req, res, next) => {
-  const { name, email, password, isAdmin } = req.body;
-
-  res.user.name = name;
-  res.user.email = email;
-
-  if (password != null) {
-    res.user.password = await bcrypt.hash(password, 10);
-  }
-
-  res.user.isAdmin = isAdmin || res.user.isAdmin;
-
-  try {
-    const updatedUser = await res.user.save();
-    res.json(updatedUser);
-  } catch (err) {
-    return next(error(400, err.message));
-  }
-});
-
-router.delete('/:id', getUser, async (req, res, next) => {
-  try {
-    await User.deleteOne({ _id: res.user.id });
-    res.json({ message: 'User deleted' });
-  } catch (err) {
-    return next(error(500, err.message));
-  }
-});
-
-router.delete('/', async (req, res, next) => {
-  try {
-    await User.deleteMany();
-    res.json({ message: 'All users deleted' });
-  } catch (err) {
-    return next(error(500, err.message));
-  }
-});
-
-router.post('/invalid', async (req, res, next) => {
-  const invalidUser = new User({
-    name: '',
-    email: 'invalid-email',
-    password: 'short',
-    isAdmin: 'notabool'
-  });
-
-  try {
-    const result = await invalidUser.save();
-    res.status(201).json(result);
-  } catch (err) {
-    return next(error(400, err.message));
-  }
-});
-
-async function getUser(req, res, next) {
-  if (req.params.id.length !== 24) {
-    return next(error(400, 'Invalid user ID'));
-  }
-  let user;
-  try {
-    user = await User.findById(req.params.id);
-    if (user == null) {
-      return next(error(404, 'User not found'));
-    }
-  } catch (err) {
-    return next(error(500, err.message));
-  }
-
-  res.user = user;
-  next();
-}
+router.get('/', getUsers);
+router.get('/:id', validateUser, getUser);
+router.post('/', createUser);
+router.patch('/:id', validateUser, updateUser);
+router.put('/:id', validateUser, replaceUser);
+router.delete('/:id', validateUser, deleteUser);
+router.delete('/', deleteAllUsers);
+router.post('/invalid', createInvalidUser);
 
 export default router;
